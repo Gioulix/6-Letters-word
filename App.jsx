f96b2b0c81cd2909ad560501f429621fa3742d94
Gioulix <gioulixtaf@gmail.com>
2025-04-21 21:55:38 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Word from "./Word.jsx";
+
+function App() {
+  return (
+    <>
+      <Word />
+    </>
+  );
+}
+
+export default App;
